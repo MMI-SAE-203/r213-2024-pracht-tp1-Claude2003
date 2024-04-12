348ec39c170bf56ecfd2c1fb2f4373e6ad88b855
claudesamb <144018008+claudesamb@users.noreply.github.com>
2024-04-12 10:09:55 +0200

    cache

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,33 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-//
+
+import { ref } from "vue";
+
+
+ 
+const maVar = ref(true) 
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+  <button @click="maVar = 
+  false"> cache  </button>
+
+  <button @click="maVar =
+  true"> pas cache 
+  </button>
+
+  <h1 v-show="maVar" 
+  class="text-2xl">Bonjour monde !</h1>
+
+  <button @click="maVar = 
+  !maVar"> cache ou pas cache  </button>
+
+  
+  
+  <h1 v-show="maVar" 
+  class="text-2xl">Bonjour monde !</h1>
+
+
+
+<p> mavar : {{maVar}} </p>
 </template>
